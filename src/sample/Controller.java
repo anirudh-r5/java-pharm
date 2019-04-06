8f80e62a14e3b8793b6b501594b654cb75ae98e7
Anirudh Ramakrishnan <anirudh.r515@gmail.com>
2019-04-06 13:15:50 +0530

    UI for added items working

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,39 +1,63 @@
 package sample;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.Alert;
-import javafx.scene.control.TextField;
-import javafx.scene.input.KeyEvent;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 import java.sql.*;
 
 public class Controller {
     @FXML
-    TextField ID, QTY;
+    TableView colOrd;
+    @FXML
+    TableColumn colSl, colID, colName, colPrice, colQty, colAmt;
+    @FXML
+    TextField ID, QTY, name, wt, age, phone;
+    @FXML
+    RadioButton genM, genF, genO;
     Connection conn = null;
+    ObservableList<tabData> list = FXCollections.observableArrayList();
 
     public void initialize() {
         System.out.println("UI Controller working!!!");
         connect();
-        System.out.println("\nID\t\tName\t\tPrice\t\tQty\t\tAmt\n");
+//        System.out.println("\nSl\t\tID\t\tName\t\tPrice\t\tQty\t\tAmt\nNo.\n");
     }
 
     @FXML
     private void printOut(tabData td, int q) {
         if (td.getName() == null) {
+            tabData.slTrack--;
             Alert alert = new Alert(Alert.AlertType.ERROR);
             alert.setTitle("ERROR");
-            alert.setHeaderText(null);
-            alert.setContentText("Invalid Item ID. Please enter a valid item ID");
+            alert.setHeaderText("Invalid Item ID");
+            alert.setContentText("Please enter a valid item ID");
             alert.show();
         } else if (q > td.getQty()) {
+            tabData.slTrack--;
             Alert alert = new Alert(Alert.AlertType.ERROR);
             alert.setTitle("ERROR");
             alert.setHeaderText("No stock");
             alert.setContentText("The entered quantity is unavailable");
             alert.show();
-        } else
-            System.out.println(td.getItemID() + "\t\t" + td.getName() + "\t\t" + td.getPrice() + "\t\t" + td.getQty() + "\t\t" + td.getAmt() + "\n");
+        } else {
+            td.setQty(q);
+            td.setAmt(td.getQty() * td.getPrice());
+            td.setSl(tabData.slTrack);
+            colSl.setCellValueFactory(new PropertyValueFactory<tabData, Integer>("Sl"));
+            colID.setCellValueFactory(new PropertyValueFactory<tabData, Integer>("ItemID"));
+            colQty.setCellValueFactory(new PropertyValueFactory<tabData, Integer>("Qty"));
+            colAmt.setCellValueFactory(new PropertyValueFactory<tabData, Integer>("Amt"));
+            colPrice.setCellValueFactory(new PropertyValueFactory<tabData, Integer>("Price"));
+            colName.setCellValueFactory(new PropertyValueFactory<tabData, String>("Name"));
+            list.add(td);
+            colOrd.setItems(list);
+            colOrd.setVisible(true);
+//            System.out.println(tabData.slTrack + "\t\t" + td.getItemID() + "\t\t" + td.getName() + "\t\t" +
+//                    td.getPrice() + "\t\t" + td.getQty() + "\t\t" + td.getAmt() + "\n");
+        }
     }
 
     private void connect() {
@@ -55,18 +79,18 @@ public class Controller {
 
     @FXML
     private void query(int i, int q) {
-        String sql = "SELECT ID, name, price, qty " + "FROM stocks where ID = ?";
+        String sql = "SELECT ID, name, price, qty FROM stocks where ID = ?";
         tabData td = new tabData();
         ResultSet rs = null;
         try (PreparedStatement pstmt = conn.prepareStatement(sql)) {
             pstmt.setInt(1, i);
             rs = pstmt.executeQuery();
             while (rs.next()) {
+                td.setSl(tabData.slTrack++);
                 td.setItemID(rs.getInt("ID"));
                 td.setName(rs.getString("name"));
                 td.setPrice(rs.getInt("price"));
                 td.setQty(rs.getInt("qty"));
-                td.setAmt((td.getQty() * td.getPrice()));
             }
         } catch (SQLException e) {
             System.out.println(e.getMessage());
@@ -76,7 +100,6 @@ public class Controller {
     }
 
     @FXML
-    private void allSubmit(KeyEvent ke) {
-
+    private void allSubmit() {
     }
 }
